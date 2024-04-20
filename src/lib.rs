182739d29f03703a7142f95779783c2d97212e3b
hnsyprst <63199729+hnsyprst@users.noreply.github.com>
2024-04-20 18:06:21 +0100

    Implement `State.render()`

--- src/lib.rs
+++ src/lib.rs
@@ -108,11 +108,45 @@ impl State {
     }
 
     fn update(&mut self) {
-        todo!()
+        // todo!()
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
+        // Get a frame to render to
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        // Create a `CommandEncoder` to create the store commands in a command buffer that will be sent to the GPU
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        // Create a `RenderPass` to clear and render the frame
+        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        });
+        drop(render_pass); // Need to drop `render_pass` to release the mutable borrow of `encoder` so we can call `encoder.finish()`
+
+        // `Queue.submit()` will accept anything that implements `IntoIter``, so we wrap `encoder.finish()` up in `std::iter::once`
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
 
@@ -153,32 +187,49 @@ pub async fn run() {
 
     let mut state =  State::new(window).await;
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == state.window().id() => if !state.input(event) { 
-            match event {
-                WindowEvent::CloseRequested | 
-                WindowEvent::KeyboardInput {
-                    input: KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
+    event_loop.run(move |event, _, control_flow|
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.window().id() => if !state.input(event) { 
+                match event {
+                    WindowEvent::CloseRequested | 
+                    WindowEvent::KeyboardInput {
+                        input: KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
                         ..
-                    },
-                    ..
-                } => {
-                    *control_flow = ControlFlow::Exit;
+                    } => {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged{ new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
                 }
-                WindowEvent::Resized(physical_size) => {
-                    state.resize(*physical_size);
-                }
-                WindowEvent::ScaleFactorChanged{ new_inner_size, .. } => {
-                    state.resize(**new_inner_size);
-                }
-                _ => {}
             }
-        }
-        _ => {}
-    });
+            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+                state.update();
+                match state.render() {
+                    Ok(_) => {}
+                    // The surface is lost, so we need to reconfigure the surface
+                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                    // The system is OOM, so let's just quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // Anything else should be resolved by the next frame, so print an error and move on
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+            Event::MainEventsCleared => {
+                // RedrawRequested will only trigger once unless we manually request it.
+                state.window().request_redraw();
+            }
+            _ => {}
+        });
 }
