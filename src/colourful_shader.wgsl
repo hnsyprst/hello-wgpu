5656083a95014f9c6448d8b74431b3e441ec109d
hnsyprst <63199729+hnsyprst@users.noreply.github.com>
2024-04-23 21:35:51 +0100

    Use texture in shader

--- src/colourful_shader.wgsl
+++ src/colourful_shader.wgsl
@@ -23,5 +23,5 @@ fn vs_main(
 // Fragment shader
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(in.clip_position.x, in.clip_position.y, 1.0, 1.0);
+    return vec4<f32>(in.color, 1.0);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ use winit::{
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: [f32; 3],
-    color: [f32; 3],
+    tex_coords: [f32; 2],
 }
 
 impl Vertex {
@@ -32,7 +32,7 @@ impl Vertex {
                 wgpu::VertexAttribute {
                     offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
+                    format: wgpu::VertexFormat::Float32x2,
                 }
             ]
         }
@@ -42,11 +42,11 @@ impl Vertex {
 // Make a triangle
 const VERTICES: &[Vertex] = &[
     // Vertices arranged in counter-clockwise order
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.5, 0.0] },
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.0, 0.5, 0.5] },
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.0, 0.0, 0.5] },
-    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.0] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.99240386], },
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.56958647], },
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.05060294], },
+    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.1526709], },
+    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.7347359], },
 ];
 
 const INDICES: &[u16] = &[
