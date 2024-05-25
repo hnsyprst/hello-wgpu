0c14065835c194c26395d7910592538ce986f965
hnsyprst <63199729+hnsyprst@users.noreply.github.com>
2024-05-25 14:14:00 +0100

    Implement diffuse lighting in primary shader

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ struct Song {
 #[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 struct RawInstance {
     model: [[f32; 4]; 4],
+    normal: [[f32; 3]; 3],
 }
 
 impl RawInstance {
@@ -44,7 +45,8 @@ impl RawInstance {
             array_stride: mem::size_of::<RawInstance>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Instance,
             attributes: &[
-                // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
+                // model vec4s
+                // A mat4x4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
                 // for each vec4. We'll have to reassemble the mat4 in the shader.
                 wgpu::VertexAttribute {
                     offset: 0,
@@ -66,6 +68,23 @@ impl RawInstance {
                     shader_location: 8,
                     format: wgpu::VertexFormat::Float32x4,
                 },
+                // normal vec3s
+                // See above comment about mat4x4s, we'll have a similar situation in the shader here with mat3x3/vec3s.
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 16]>() as wgpu::BufferAddress,
+                    shader_location: 9,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 19]>() as wgpu::BufferAddress,
+                    shader_location: 10,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 22]>() as wgpu::BufferAddress,
+                    shader_location:11,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
             ],
             }
     }
@@ -81,6 +100,7 @@ impl Instance {
     fn to_raw(&self) -> RawInstance {
         RawInstance {
             model: (cgmath:: Matrix4::from_translation(self.position) * cgmath::Matrix4::from(self.rotation)).into(),
+            normal: cgmath::Matrix3::from(self.rotation).into(),
         }
     }
 }
