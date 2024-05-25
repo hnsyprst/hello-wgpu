439a208f0ad89e07dbdb60854aea9c8e73cffa90
hnsyprst <63199729+hnsyprst@users.noreply.github.com>
2024-05-25 14:31:38 +0100

    Implement specular lighting in primary shader

--- src/camera.rs
+++ src/camera.rs
@@ -41,6 +41,7 @@ impl Camera {
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct CameraUniform {
+    view_position: [f32; 4],
     view_proj: [[f32; 4]; 4],
 }
 
@@ -48,11 +49,14 @@ impl CameraUniform {
     pub fn new() -> Self {
         use cgmath::SquareMatrix;
         Self {
+            view_position: [0.0; 4],
             view_proj: cgmath::Matrix4::identity().into()
         }
     }
 
     pub fn update_view_proj(&mut self, camera: &Camera) {
+        // We're using Vector4 because of the uniforms 16 byte spacing requirement
+        self.view_position = camera.eye.to_homogeneous().into();
         self.view_proj = camera.build_view_projection_matrix().into();
     }
 }
