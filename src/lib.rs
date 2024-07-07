053f4c323133df395224e7bf9f961ec6b47dd72e
hnsyprst <63199729+hnsyprst@users.noreply.github.com>
2024-07-07 14:13:23 +0100

    Multiple render passes

--- src/lib.rs
+++ src/lib.rs
@@ -39,8 +39,10 @@ struct Song {
 }
 
 struct State {
+    basic_pass: render_pass::basic::BasicPass,
+    basic_objects: Vec<object::Object>,
     phong_pass: render_pass::phong::PhongPass,
-    objects: Vec<object::Object>,
+    phong_objects: Vec<object::Object>,
     // clear_color: wgpu::Color,
     camera: camera::Camera,
     camera_controller: camera::CameraController,
@@ -81,12 +83,29 @@ impl State {
         
         let camera_controller = camera::CameraController::new(0.2);
 
+        let basic_pass = render_pass::basic::BasicPass::new(&app_data.device, &app_data.queue, &app_data.config, &camera);
         let phong_pass = render_pass::phong::PhongPass::new(&app_data.device, &app_data.queue, &app_data.config, &camera);
 
         // Load models
+        let light_model = resources::load_model("lightbulb_2.obj", &app_data.device, &app_data.queue, &phong_pass.texture_bind_group_layout).await.unwrap();
         let cube_model = resources::load_model("cube.obj", &app_data.device, &app_data.queue, &phong_pass.texture_bind_group_layout).await.unwrap();
         let ferris_model = resources::load_model("ferris.obj", &app_data.device, &app_data.queue, &phong_pass.texture_bind_group_layout).await.unwrap();
-        // Set up instances
+        
+        // Set up instances for basic pass
+        let light_instance = vec![{
+            let position = cgmath::Vector3::from(basic_pass.light_uniform.position);
+            let rotation = if position.is_zero() {
+                cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0))
+            } else {
+                cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(0.0))
+            };
+            let rotation_speed: f32 = 0.0;
+            Instance { position, rotation, rotation_speed }
+        }];
+        let basic_objects = vec![
+            object::Object{ model: light_model, instances: light_instance },
+        ];
+        // Set up instances for phong pass
         // Note: if new instances are added at runtime, both `instance_buffer` and `camera_bind_group` must be recreated
         let songs: Vec<Song> = resources::load_json::<Song>("coords.json").await.unwrap();
         const SPACE_BETWEEN: f32 = 5.0;
@@ -114,14 +133,16 @@ impl State {
             let rotation_speed: f32 = rng.gen_range(-0.5..0.5);
             Instance { position, rotation, rotation_speed }
         }];
-        let objects = vec![
+        let phong_objects = vec![
             object::Object{ model: cube_model, instances: cube_instances },
             object::Object{ model: ferris_model, instances: ferris_instance },
         ];
 
         Self {
+            basic_pass,
+            basic_objects,
             phong_pass,
-            objects,
+            phong_objects,
             // clear_color,
             camera,
             camera_controller,
@@ -172,7 +193,7 @@ fn update(
     state: &mut State,
 ) {
     // Move instances
-    for object in state.objects.iter_mut() {
+    for object in state.phong_objects.iter_mut() {
         object.instances = object.instances.iter().map(|instance| {
             let position = instance.position;
             let rotation_speed = instance.rotation_speed;
@@ -186,10 +207,14 @@ fn update(
     state.phong_pass.light_uniform.position = (
         cgmath::Quaternion::from_axis_angle((0.0, 1.0, 0.0).into(), cgmath::Deg(5.0)) * light_position
     ).into();
+    state.basic_pass.light_uniform.position = (
+        cgmath::Quaternion::from_axis_angle((0.0, 1.0, 0.0).into(), cgmath::Deg(5.0)) * light_position
+    ).into();
 
     // Move camera
     state.camera_controller.update_camera(&mut state.camera);
     state.phong_pass.camera_uniform.update_view_proj(&state.camera);
+    state.basic_pass.camera_uniform.update_view_proj(&state.camera);
     // // Despite not explicitly using a staging buffer, this is still pretty performant (apparently) https://github.com/gfx-rs/wgpu/discussions/1438#discussioncomment-345473
     // app_data.queue.write_buffer(&state.camera.uniform_buffer(), 0, bytemuck::cast_slice(&[*state.camera.uniform()]));
 }
@@ -200,13 +225,23 @@ fn render(
     view: wgpu::TextureView,
     mut encoder: wgpu::CommandEncoder,
 ) {
-    // TODO: Handle this error
-    let _ = state.phong_pass.draw(
+    encoder = state.phong_pass.draw(
         app_data,
-        view,
+        &view,
         encoder,
-        &state.objects,
-    );
+        &state.phong_objects,
+        None,
+    ).unwrap();
+    encoder = state.basic_pass.draw(
+        app_data,
+        &view,
+        encoder,
+        &state.basic_objects,
+        Some(&state.phong_pass.depth_texture),
+    ).unwrap();
+
+    // `Queue.submit()` will accept anything that implements `IntoIter`, so we wrap `encoder.finish()` up in `std::iter::once`
+    app_data.queue.submit(std::iter::once(encoder.finish()));
 }
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
